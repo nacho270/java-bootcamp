e6368a91a51ceb3396cd42c1e3a8fae226f6024b
Fer Campo <fernandocampow@gmail.com>
2015-11-03 15:04:39 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        House homeSweetHome = new House.Builder(3).addBathroom().addKitchen().addLivingRoom().addLivingRoom().build();
-        System.out.println(homeSweetHome);
-    }
-}
